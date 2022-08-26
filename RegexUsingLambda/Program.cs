d73bf86eccab74bedd528b186433520c9a5d83ce
Kundan <kundan kamble>
2022-08-25 19:19:58 -0800

    Validate Last Name

--- RegexUsingLambda/Program.cs
+++ RegexUsingLambda/Program.cs
@@ -4,10 +4,16 @@
     {
         public static void Main(string[] args)
         {
+            //UC1
             UserRegistration userRegistration = new UserRegistration();
             Console.WriteLine("Enter First name");
             string fName = Console.ReadLine();
             userRegistration.ValidFirstName(fName);
+
+            //UC2
+            Console.WriteLine("Enter Last name");
+            string LastName = Console.ReadLine();
+            userRegistration.validLastName(LastName);
         }
     }
 }
--- RegexUsingLambda/Validation.cs
+++ RegexUsingLambda/Validation.cs
@@ -10,8 +10,7 @@ namespace RegexUsingLambda
     public class UserRegistration
     {
         public Regex FirstNameR = new Regex("^[A-Z]{1}[a-zA-Z]{2,}$");
-
-
+        public Regex LastNameR = new Regex("^[A-Z]{1}[a-zA-Z]{2,}$");
         public string ValidFirstName(string FirstName)
         {
             bool FirstNamePattern(string FirstNamePattern) => FirstNameR.IsMatch(FirstName);
@@ -29,6 +28,23 @@ namespace RegexUsingLambda
                 Console.WriteLine("First Name is valid");
             }
             return "FirstName;
+        }
+
+        public string validLastName(string LastName)
+        {
+            bool LastNamePattern(string LastNamePattern) => LastNameR.IsMatch(LastName);
+            bool result = LastNamePattern(LastName);
+            
+                if (result == false)
+                {
+                Console.WriteLine("Last Name is Invalid");
+                }
+               
+                else
+                {
+                 Console.WriteLine("Last Name is valid");
+                }
+                 return LastName;
         }
     }
 }
