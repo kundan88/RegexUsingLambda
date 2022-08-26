86cdf91e7897213cbe42a72e95991f9e4a061315
Kundan <kundan kamble>
2022-08-25 19:17:33 -0800

    Validate First Name

---
+++ RegexUsingLambda/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RegexUsingLambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            Console.WriteLine("Enter First name");
+            string fName = Console.ReadLine();
+            userRegistration.ValidFirstName(fName);
+        }
+    }
+}
---
+++ RegexUsingLambda/Validation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUsingLambda
+{
+    public class UserRegistration
+    {
+        public Regex FirstNameR = new Regex("^[A-Z]{1}[a-zA-Z]{2,}$");
+
+
+        public string ValidFirstName(string FirstName)
+        {
+            bool FirstNamePattern(string FirstNamePattern) => FirstNameR.IsMatch(FirstName);
+            bool result = FirstNamePattern(FirstName);
+
+
+            if (result == false)
+            {
+                Console.WriteLine("First Name is Invalid");
+
+            }
+
+            else
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            return "FirstName;
+        }
+    }
+}
