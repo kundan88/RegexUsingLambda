17c82d20ffc01a7d151b6d9045e0b524192ac1e1
Kundan <kundan kamble>
2022-08-25 20:02:34 -0800

    Refactor Code to use Lambda fuction to validate

---
+++ RegexUsingLambda/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace RegexUsingLambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            int choice;
+            do
+            {
+                Console.WriteLine("select your choice");
+                Console.WriteLine("1 for FirstName");
+                Console.WriteLine("2 for LaststName");
+                Console.WriteLine("3 for Email");
+                Console.WriteLine("4 for MobileNumber");
+                Console.WriteLine("5 for Password");
+                Console.WriteLine("6 for Password at least one Upper case");
+                Console.WriteLine("7 for Password at least one Number");
+                Console.WriteLine("8 for Password at least one special char");
+                Console.WriteLine("9 for Check Email Validation");
+                Console.WriteLine("10 for Exit");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First name");
+                        string fName = Console.ReadLine();
+                        userRegistration.ValidFirstName(fName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Last name");
+                        string LastName = Console.ReadLine();
+                        userRegistration.validLastName(LastName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Email");
+                        string Email = Console.ReadLine();
+                        userRegistration.validEmail(Email);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Mobile Number");
+                        string MobileNo = Console.ReadLine();
+                        userRegistration.validMobileNo(MobileNo);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Password");
+                        string Password = Console.ReadLine();
+                        userRegistration.validPassword(Password);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter Password");
+                        string Password_1 = Console.ReadLine();
+                        userRegistration.validPasswordR2(Password_1);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter Password");
+                        string Password_2 = Console.ReadLine();
+                        userRegistration.validPasswordR3(Password_2);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter Password");
+                        string Password_4 = Console.ReadLine();
+                        userRegistration.validPasswordR3(Password_4);
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter Password");
+                        string CheckEmail = Console.ReadLine();
+                        userRegistration.validCheckEmailSample(CheckEmail);
+                        break;
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                } 
+            } while (choice != 10);
+
+
+
+        }
+
+
+    }
+}
---
+++ RegexUsingLambda/Validation.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUsingLambda
+{
+    public class UserRegistration
+    {
+        public Regex FirstNameR = new Regex("^[A-Z]{1}[a-zA-Z]{2,}$");
+        public Regex LastNameR = new Regex("^[A-Z]{1}[a-zA-Z]{2,}$");
+        public Regex EmailR = new Regex("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
+        public Regex MobileNoR = new Regex("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$");
+        public Regex PasswordR = new Regex("^[a-zA-Z0-9]{8,}$");
+        public Regex PasswordR2 = new Regex("^(?=.*[A-Z])[a-zA-Z0-9]{8,}$");
+        public Regex PasswordR3 = new Regex("^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$");
+        public Regex PasswordR4 = new Regex("^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9]{8,}$");
+        public Regex CheckEmailR = new Regex("^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$");
+
+
+        public string ValidFirstName(string FirstName)
+        {
+            bool FirstNamePattern(string FirstNamePattern) => FirstNameR.IsMatch(FirstName);
+            bool result = FirstNamePattern(FirstName);
+
+
+            if (result == false)
+            {
+                Console.WriteLine("First Name is Invalid");
+
+            }
+
+            else
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            return FirstName;
+        }
+
+        public string validLastName(string LastName)
+        {
+            bool LastNamePattern(string LastNamePattern) => LastNameR.IsMatch(LastName);
+            bool result = LastNamePattern(LastName);
+            
+                if (result == false)
+                {
+                Console.WriteLine("Last Name is Invalid");
+                }
+               
+                else
+                {
+                 Console.WriteLine("Last Name is valid");
+                }
+               return LastName;
+        }
+
+        public string validEmail(string Email)
+        {
+            bool EmailPattern(string EmailPattern) => EmailR.IsMatch(Email);
+            bool result = EmailPattern(Email);
+            
+                if (result == false)
+                {
+                    Console.WriteLine("EmailId is Invalid");
+                }
+                
+                else
+                {
+                Console.WriteLine("EmailId is valid");
+                }
+               return Email;
+        }
+
+        public string validMobileNo(string MobileNo)
+        {
+            bool MobileNoPattern(string MobileNoPattern) => MobileNoR.IsMatch(MobileNo);
+            bool result = MobileNoPattern(MobileNo);
+            
+                if (result == false)
+                {
+                Console.WriteLine("Mobile Number is not Valid");
+                }
+               else            
+               {
+                Console.WriteLine("Mobile Number is Valid");
+            }
+            return MobileNo;
+        }
+
+        public string validPassword(string Password)
+        {
+            bool PasswordPattern(string PasswordPattern) => PasswordR.IsMatch(Password);
+            bool result = PasswordPattern(Password);
+            
+            
+                if (result == false)
+                {
+                Console.WriteLine("Password is Invalid");
+                }
+                
+                else
+                {
+                Console.WriteLine("Password is valid");
+                }
+               return Password;
+        }
+        public string validPasswordR2(string Password2)
+        {
+            bool PasswordR2Pattern(string PasswordR2Pattern) => PasswordR.IsMatch(Password2);
+            bool result = PasswordR2Pattern(Password2);
+            
+                if (result == false)
+                {
+                    Console.WriteLine("Password is Invalid");
+                }
+                else
+                {
+                    Console.WriteLine("Password is valid");
+                }
+            return Password2;
+            
+        }
+        public string validPasswordR3(string Password3)
+        {
+            bool PasswordR3Pattern(string PasswordR3Pattern) => PasswordR.IsMatch(Password3);
+            bool result = PasswordR3Pattern(Password3);
+            
+                if (result == false)
+                {
+                Console.WriteLine("Password is Invalid");
+                }
+                            
+               else
+               {
+                Console.WriteLine("Password is Invalid");
+               }
+               return Password3;
+        }
+
+        public string validPasswordR4(string Password_4)
+        {
+            bool PasswordR4Pattern(string PasswordR4Pattern) => PasswordR.IsMatch(Password_4);
+            bool result = PasswordR4Pattern(Password_4);
+            
+                if (result == false)
+                {
+                    Console.WriteLine("Password is Invalid");
+                }               
+            
+                else
+                {
+                    Console.WriteLine("Password is valid");
+                }
+               return Password_4;
+        }
+        public string validCheckEmailSample(string CheckEmail)
+        {
+            bool CheckEmailPattern(string CheckEmailPattern) => CheckEmailR.IsMatch(CheckEmail);
+            bool result = CheckEmailPattern(CheckEmail);
+            
+                if (result == false)
+                {
+                Console.WriteLine("Email is Invalid");
+                }
+                           
+                else
+                {
+                  Console.WriteLine("Email is valid");
+                }
+                return CheckEmail;
+        }
+    }
+}
